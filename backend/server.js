f69fb7f59e7517574f2af481a556ebfeec0d1448
hemant <hunnyybarasker01@gmail.com>
2025-03-20 14:47:55 +0530

    backend completed

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
+import cookieParser from "cookie-parser";  //package imports
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
-import connectToMongoDB from "./db/connectToMongoDB.js";
+import userRoutes from "./routes/user.routes.js";
+
+
+
+
+import connectToMongoDB from "./db/connectToMongoDB.js"; //database imports
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,8 +18,12 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 app.use(express.json()); //to parse the incoming request with json payloads(from req.body )
 app.use(cookieParser()); //to parse the incoming request with
+
+
 app.use("/api/auth", authRoutes);
 app.use ("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
 
 // app.get("/", (req, res) => {
 //     //root route http://localhost:5000/
